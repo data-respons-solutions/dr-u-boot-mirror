72791d546b03f572db24e98bf96d26c95ccd7140
Mikko Salomäki <ms@datarespons.se>
2022-08-31 17:14:48 +0200

    cleanup unused functions

--- include/configs/vhgw.h
+++ include/configs/vhgw.h
@@ -3,16 +3,12 @@
  * Copyright 2018 NXP
  */
 
-#ifndef __IMX8MN_EVK_H
-#define __IMX8MN_EVK_H
+#ifndef __VHGW_H
+#define __VHGW_H
 
 #include <linux/sizes.h>
 #include <linux/stringify.h>
 #include <asm/arch/imx-regs.h>
-#include "imx_env.h"
-
-#define CONFIG_SYS_BOOTM_LEN		(32 * SZ_1M)
-
 
 #define CONFIG_SPL_MAX_SIZE		(208 * 1024)
 #define CONFIG_SYS_MONITOR_LEN		(512 * 1024)
@@ -59,117 +55,39 @@
 #define IMX_FEC_BASE			0x30BE0000
 #endif
 
-#ifdef CONFIG_DISTRO_DEFAULTS
-#define BOOT_TARGET_DEVICES(func) \
-	func(USB, usb, 0) \
-	func(MMC, mmc, 1) \
-	func(MMC, mmc, 2)
+/* nxp shenanigans */
+#define CONFIG_SYS_MMC_ENV_DEV 2 /* Can't disable arch/arm/mach-imx/mmc_env.c */
+#undef is_boot_from_usb /* If defined overwrites bootcmd */
 
-#include <config_distro_bootcmd.h>
-#else
-#define BOOTENV
-#endif
+/* boot configuration */
+#define CONFIG_SYS_BOOTM_LEN (1024 * 1024 * 512) /* fit image might include kernel and ramdisk, increase size */
+#define SYS_BOOT_IFACE "mmc" /* Required by android_boot */
+#define SYS_BOOT_DEV 2 /* Required by android_boot */
+#define FIT_ADDR 0x43400000
+#define FIT_IMAGE "/boot/fitImage"
 
-/*
- * Another approach is add the clocks for inmates into clks_init_on
- * in clk-imx8mm.c, then clk_ingore_unused could be removed.
- */
-#ifdef CONFIG_TARGET_IMX8MN_DDR4_EVK
-#define JH_ROOT_DTB	"imx8mn-ddr4-evk-root.dtb"
-#else
-#define JH_ROOT_DTB	"imx8mn-evk-root.dtb"
-#endif
-
-#define JAILHOUSE_ENV \
-	"jh_clk= \0 " \
-	"jh_root_dtb=" JH_ROOT_DTB "\0" \
-	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdtfile ${jh_root_dtb};" \
-		"setenv jh_clk clk_ignore_unused mem=1212MB; " \
-			   "if run loadimage; then " \
-				   "run mmcboot; " \
-			   "else run jh_netboot; fi; \0" \
-	"jh_netboot=mw 0x303d0518 0xff; setenv fdtfile ${jh_root_dtb}; setenv jh_clk clk_ignore_unused mem=1212MB; run netboot; \0 "
-
-#define CONFIG_MFG_ENV_SETTINGS \
-	CONFIG_MFG_ENV_SETTINGS_DEFAULT \
-	"initrd_addr=0x43800000\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"emmc_dev=2\0"\
-	"sd_dev=1\0" \
+#define CONFIG_BOOTCOMMAND \
+	"echo starting boot procedure...;" \
+	"if usb start; then " \
+		"if system_load usb 0 --label TESTDRIVE; then " \
+			"usb stop;" \
+			"system_boot;" \
+		"else " \
+			"usb stop;" \
+		"fi;" \
+	"fi;" \
+	"if system_load mmc 2; then " \
+		"system_boot;" \
+	"fi;" \
+	"echo no boot device found;" \
+	"reset;"
 
 /* Initial environment variables */
 #define CONFIG_EXTRA_ENV_SETTINGS		\
-	CONFIG_MFG_ENV_SETTINGS \
-	JAILHOUSE_ENV \
-	BOOTENV \
-	"scriptaddr=0x43500000\0" \
-	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"bsp_script=boot.scr\0" \
-	"image=Image\0" \
-	"splashimage=0x50000000\0" \
-	"console=ttymxc1,115200\0" \
-	"fdt_addr_r=0x43000000\0"			\
-	"fdt_addr=0x43000000\0"			\
-	"fdt_high=0xffffffffffffffff\0"		\
-	"boot_fit=no\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	"bootm_size=0x10000000\0" \
-	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-			"bootm ${loadaddr}; " \
-		"else " \
-			"if run loadfdt; then " \
-				"booti ${loadaddr} - ${fdt_addr_r}; " \
-			"else " \
-				"echo WARN: Cannot load the DT; " \
-			"fi; " \
-		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} console=${console} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs;  " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${loadaddr} ${image}; " \
-		"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-			"bootm ${loadaddr}; " \
-		"else " \
-			"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
-				"booti ${loadaddr} - ${fdt_addr_r}; " \
-			"else " \
-				"echo WARN: Cannot load the DT; " \
-			"fi; " \
-		"fi;\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
-		"mmc dev ${mmcdev}; if mmc rescan; then " \
-		   "if run loadbootscript; then " \
-			   "run bootscript; " \
-		   "else " \
-			   "if run loadimage; then " \
-				   "run mmcboot; " \
-			   "else run netboot; " \
-			   "fi; " \
-		   "fi; " \
-	   "fi;"
+	"soc_type=imx8mm\0" /* Required by Android */
 
 
 /* Link Definitions */
-
 #define CONFIG_SYS_INIT_RAM_ADDR        0x40000000
 #define CONFIG_SYS_INIT_RAM_SIZE        0x80000
 #define CONFIG_SYS_INIT_SP_OFFSET \
@@ -223,8 +141,4 @@
 #define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CONFIG_USB_MAX_CONTROLLER_COUNT         2
 
-#ifdef CONFIG_ANDROID_SUPPORT
-#include "imx8mn_evk_android.h"
-#endif
-
-#endif
+#endif // __VHGW_H
