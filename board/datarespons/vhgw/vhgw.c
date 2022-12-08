13e37583e3f5ccb8cf810629fe22e00cfdd7a562
Mikko Salomäki <ms@datarespons.se>
2022-12-08 15:13:45 +0100

    imx8mn_evk board file to vhgw

---
+++ board/datarespons/vhgw/vhgw.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2022, Data Respons Solutions AB
+ *
+ */
+
+#include <common.h>
+#include <env.h>
+#include <init.h>
+#include <asm/global_data.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
+
+int board_late_init(void)
+{
+	return 0;
+}
