199b2f0c70f31337829265d0902a1b23cec1d8e8
Mikko Salomäki <ms@datarespons.se>
2022-12-09 11:00:20 +0100

    vhgw: enable all regulator-boot-on regulators from power_init_board()

--- board/datarespons/vhgw/vhgw.c
+++ board/datarespons/vhgw/vhgw.c
@@ -8,8 +8,21 @@
 #include <env.h>
 #include <init.h>
 #include <asm/global_data.h>
+#include <power/regulator.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+int power_init_board(void)
+{
+	int r = 0;
+
+	const int verbose = 1;
+	r = regulators_enable_boot_on(verbose);
+	if (r)
+		printf("Failed enabling boot regulators: %d\n", r);
+
+	return 0;
+}
 
 int board_init(void)
 {
