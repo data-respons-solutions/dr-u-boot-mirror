1f6b48ce3a06d2f0266eb7bf123a48cf52c73caf
Mikko Salomäki <ms@datarespons.se>
2023-05-17 15:07:16 +0200

    vhgw to v2023.04

--- include/configs/vhgw.h
+++ include/configs/vhgw.h
@@ -7,12 +7,7 @@
 #ifndef VHGW_H__
 #define VHGW_H__
 
-#include <linux/sizes.h>
 #include <asm/arch/imx-regs.h>
-
-#define CONFIG_SYS_MONITOR_LEN		SZ_512K
-#define CONFIG_SYS_UBOOT_BASE	\
-	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
 
 /* boot configuration */
 #define FIT_ADDR 0x43600000
@@ -39,9 +34,9 @@
 	"echo no boot device found;"
 
 /* Link Definitions */
-#define CONFIG_SYS_INIT_RAM_ADDR	0x40000000
-#define CONFIG_SYS_INIT_RAM_SIZE	0x200000
-#define CONFIG_SYS_SDRAM_BASE		0x40000000
+#define CFG_SYS_INIT_RAM_ADDR	0x40000000
+#define CFG_SYS_INIT_RAM_SIZE	0x200000
+#define CFG_SYS_SDRAM_BASE		0x40000000
 #define PHYS_SDRAM					0x40000000
 #define PHYS_SDRAM_SIZE				0x40000000 /* 1GB DDR */
 
