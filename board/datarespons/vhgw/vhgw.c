a73e6c560f336b42e216fd5ae7d792c542d19755
Mikko Salomäki <ms@datarespons.se>
2023-05-22 17:01:16 +0200

    vhgw: apply OPTEE dt overlay to linux dt

--- board/datarespons/vhgw/vhgw.c
+++ board/datarespons/vhgw/vhgw.c
@@ -5,11 +5,14 @@
  */
 
 #include <common.h>
+#include <stdlib.h>
 #include <env.h>
 #include <init.h>
 #include <asm/global_data.h>
 #include <power/regulator.h>
 #include <bloblist.h>
+#include <fdt_support.h>
+#include <mapmem.h>
 #include <../common/platform_header.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -52,4 +55,39 @@ int board_init(void)
 int board_late_init(void)
 {
 	return 0;
+}
+
+/*
+ * OPTEE will place dt overlay at 0x43200000.
+ * Apply the overlay before booting linux.
+ */
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	(void) bd;
+	struct fdt_header* overlay = map_sysmem(0x43200000, 0);
+	int r = 0;
+
+	if (!fdt_valid(&overlay)) {
+		printf("WARNING: optee overlay not found\n");
+		return -ENOENT;
+	}
+
+	r = fdt_shrink_to_minimum((struct fdt_header*) blob, fdt_totalsize(overlay));
+	if (r < 8096) {
+		printf("ERROR: optee fdt resize failed: %d\n", r);
+		return -ENOMEM;
+	}
+
+	/*
+	 * overlay is damaged by apply operation, use a copy.
+	 */
+	struct fdt_header* overlay_copy = malloc(fdt_totalsize(overlay));
+	if (overlay_copy == NULL) {
+		printf("ERROR: optee fdt overlay copy failed\n");
+		return -ENOMEM;
+	}
+	memcpy(overlay_copy, overlay, fdt_totalsize(overlay));
+	r = fdt_overlay_apply_verbose((struct fdt_header *) blob, overlay_copy);
+	free(overlay_copy);
+	return r;
 }
