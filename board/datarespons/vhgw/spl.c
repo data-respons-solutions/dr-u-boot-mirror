cfadcc2269be039c6c162731fc7f364e7b9a923f
Mikko Salomäki <ms@datarespons.se>
2024-02-07 13:53:31 +0100

    vhgw: don't need to enable CAAM in spl, managed by arch_misc_init()

--- board/datarespons/vhgw/spl.c
+++ board/datarespons/vhgw/spl.c
@@ -149,11 +149,6 @@ void spl_board_init(void)
 	struct udevice *dev;
 	int ret;
 
-	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
-		ret = uclass_get_device_by_driver(UCLASS_MISC, DM_DRIVER_GET(caam_jr), &dev);
-		if (ret)
-			printf("Failed to initialize caam_jr: %d\n", ret);
-	}
 	puts("Normal Boot\n");
 
 	ret = uclass_get_device_by_name(UCLASS_CLK,
